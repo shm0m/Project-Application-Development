d359f1077e6fb27ecb549a2f5f78ed2ed052b5e9
LeilaAlk <131645672+LeilaAlk@users.noreply.github.com>
2024-12-02 12:42:36 +0000

    css modif

--- DietEthic/components/Dinner.jsx
+++ DietEthic/components/Dinner.jsx
@@ -96,7 +96,7 @@ const styles = StyleSheet.create({
     color: '#888',
   },
   addButton: {
-    backgroundColor: '#4CAF50',
+    backgroundColor: '#6A4FD8',
     padding: 10,
     borderRadius: 5,
   },
